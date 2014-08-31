ebb6485924dd313a3f98974c4fa65583e93bac9e
nguyen3 <nguyen3@ualberta.ca>
2014-08-30 18:38:19 -0600

    re #6 task: Allow H2H scraping to fill tips initially before table (syntax: switch order of conditionals)

--- scraper.py
+++ scraper.py
@@ -712,13 +712,13 @@ class Scraper(webapp.RequestHandler):
                                 tip_instance = self.create_tip_change_object(tip_instance, 'total', 'stake_total_all', not tip_change_created)
                                 tip_instance = self.create_tip_change_object(tip_instance, 'stake', 'stake_all', not tip_change_created)
                             
-                            if tip_instance.wettpoint_tip_stake >= 1.0:
-                                tip_stake_changed = True
-                                tip_instance.wettpoint_tip_stake = 0.0
-                            elif tip_instance.wettpoint_tip_stake is None:
+                            if tip_instance.wettpoint_tip_stake is None:
                                 if tip_instance.wettpoint_tip_team is None and tip_instance.wettpoint_tip_total is None:
                                     tip_stake_changed = True
                                     tip_instance.wettpoint_tip_stake = 0.0
+                            elif tip_instance.wettpoint_tip_stake >= 1.0:
+                                tip_stake_changed = True
+                                tip_instance.wettpoint_tip_stake = 0.0
                                 
                             break
                     

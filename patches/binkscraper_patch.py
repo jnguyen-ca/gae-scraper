0124869f8bad352cab5dd7b19b0b52d458c38af6
nguyen3 <nguyen3@ualberta.ca>
2014-09-29 11:51:59 -0600

    re #43 task: 3.1.0 patch to find all doubleheaders and duplicate games

--- patches/binkscraper_patch.py
+++ patches/binkscraper_patch.py
@@ -10,12 +10,28 @@ from mapreduce import operation
 from patch_3_1_0_doubleheader_tip import patch_3_1_0_doubleheader_tip
 
 def run_patch(tip_instance):
-    if tip_instance.game_sport == 'Baseball':
-        result = patch_3_1_0_doubleheader_tip(tip_instance)
-        
-        if result:
-            yield operation.counters.Increment(result)
-        else:
-            yield operation.counters.Increment('Pass')
+    result = patch_3_1_0_doubleheader_tip(tip_instance)
+    
+    yield operation.counters.Increment(tip_instance.game_sport)
+    
+    size_team = size_total_no = size_total = size_spread_no = size_spread = 0
+    
+    if tip_instance.team_lines:
+        size_team = len(tip_instance.team_lines)
+    if tip_instance.total_no:
+        size_total_no = len(tip_instance.total_no)
+    if tip_instance.total_lines:
+        size_total = len(tip_instance.total_lines)
+    if tip_instance.spread_no:
+        size_spread_no = len(tip_instance.spread_no)
+    if tip_instance.spread_lines:
+        size_spread = len(tip_instance.spread_lines)
+    
+    total_size = size_team + size_total_no + size_total + size_spread_no + size_spread
+    
+    yield operation.counters.Increment(tip_instance.game_sport + ' Text', total_size)
+    
+    if result:
+        yield operation.counters.Increment(result)
     else:
-        yield operation.counters.Increment('Sport Pass')
+        yield operation.counters.Increment('Pass')
--- patches/patch_3_1_0_doubleheader_tip.py
+++ patches/patch_3_1_0_doubleheader_tip.py
@@ -9,18 +9,17 @@ from models import Tip
 def patch_3_1_0_doubleheader_tip(tip_instance):
     game_time = tip_instance.date - datetime.timedelta(hours = 6)
     
-    start_day = datetime.datetime.combine(game_time, datetime.time())
-    end_day = datetime.datetime.combine(game_time, datetime.time(hour=23,minute=59,second=59,microsecond=59))
+    start_day = datetime.datetime.combine(game_time, datetime.time()) + datetime.timedelta(hours = 6)
+    end_day = datetime.datetime.combine(game_time, datetime.time(hour=23,minute=59,second=59,microsecond=59)) + datetime.timedelta(hours = 6)
     
-    query = Tip.gql('WHERE date != :1 AND date >= :2 AND date <= :3 AND game_team_away = :4 AND game_team_home = :5',
-                    tip_instance.date,
+    query = Tip.gql('WHERE date >= :1 AND date <= :2 AND game_team_away = :3 AND game_team_home = :4',
                     start_day,
                     end_day,
                     tip_instance.game_team_away,
                     tip_instance.game_team_home
                     )
     
-    if query.count() != 0:
-        return game_time.strftime('%d.%m.%Y %H:%M')+': '+tip_instance.game_team_away+' @ '+tip_instance.game_team_home
-    else:
-        return False
+    if query.count() > 1:
+        return tip_instance.date.strftime('%d.%m.%Y')+': '+tip_instance.game_team_away+' @ '+tip_instance.game_team_home
+            
+    return False

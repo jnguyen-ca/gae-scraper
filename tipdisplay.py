72fae35ad84367b1dac5f046cdfb4272c8a3c52e
nguyen3 <nguyen3@ualberta.ca>
2014-11-08 14:42:15 -0700

    re #1: update tipdisplay module - fix bug where display would crash if event with invalid team was in db - fix events with no money lines not displaying last scrape date - add id to each event - add background to event if same day and event still has no line - list_next_games function should be part of tipdisplay module

--- tipdisplay.py
+++ tipdisplay.py
@@ -31,21 +31,23 @@ class TipDisplay(webapp.RequestHandler):
         self.jsheader = []
         self.scriptheader = []
         
+        self.styleheader.append('<style>')
+        
 #         self.jsheader.append(
 # '''<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>''')
         self.styleheader.append(
-'''<style>
-    .game_time-MST {width: 12%;}
-    .upcoming_game {margin: 6px 0;}
-    .upcoming_game > span {display: inline-block; margin-right: 12px; min-width: 60px;}
-    .upcoming_game .participants {width: 22%; text-overflow: ellipsis;}
-    .upcoming_game .tip-team {text-overflow: ellipsis;}
-    .upcoming_game .tip-date {width: 10%;}
-    .upcoming_game .tip-line-adjusted_win, .upcoming_game .tip-line-adjusted_loss {background: lightgrey; text-align: center;}
-    .upcoming_game .today {font-weight: bold;}
-    .upcoming_game .closing {color: red;}
-    .upcoming_game .pending {color: green;}
-</style>''')
+'''
+.game_time-MST {width: 12%;}
+.upcoming_game {margin: 6px 0;}
+.upcoming_game > span {display: inline-block; margin-right: 12px; min-width: 60px;}
+.upcoming_game .participants {width: 22%; text-overflow: ellipsis;}
+.upcoming_game .tip-team {text-overflow: ellipsis;}
+.upcoming_game .tip-date {width: 10%;}
+.upcoming_game .tip-line-adjusted_win, .upcoming_game .tip-line-adjusted_loss {background: lightgrey; text-align: center;}
+.upcoming_game .today {font-weight: bold;}
+.upcoming_game .closing {color: red;}
+.upcoming_game .pending {color: green;}
+''')
         self.html.append('<body>')
         
         #TODO: move to tipanalysis
@@ -133,9 +135,10 @@ class TipDisplay(webapp.RequestHandler):
             
             # display all non-archived tips
             self.html.append("<div class='league_key'><b>%(league_key)s</b> <a href='%(wettpoint_table)s'>Table</a></div>" % locals())
-            self.html.append(list_next_games(league_key, not_archived_tips_values))
+            self.list_next_games(league_key, not_archived_tips_values)
         
         self.html.append('</body></html>')    
+        self.styleheader.append('</style>')
             
         html = []
         html.append('''<html><head>''')
@@ -153,64 +156,83 @@ class TipDisplay(webapp.RequestHandler):
         
         logging.debug('Total Reads: '+str(self.DATASTORE_READS)+', Total Writes: '+str(self.DATASTORE_PUTS))
         
-def list_next_games(league, not_archived_tips):
-    next_games_html = []
-    
-    local_timezone = pytz.timezone(constants.TIMEZONE_LOCAL)
-    
-    game_count = 0
-    next_games_html.append('<div class="upcoming_games">')
-    for tip_instance in not_archived_tips:
-        current_time = datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(local_timezone)
-        game_time = tip_instance.date.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+    def list_next_games(self, league, not_archived_tips):
+        next_games_html = []
         
-        date_class = 'game_time-MST'
-        if current_time.date().day == game_time.date().day:
-            date_class += ' today'
-        elif (game_time.date() - current_time.date()).days == 1:
-            date_class += ' tomorrow'
-        if game_time - current_time < timedelta(hours=2):
-            date_class += ' closing'
-        if tip_instance.elapsed == True:
-            date_class += ' pending'
+        local_timezone = pytz.timezone(constants.TIMEZONE_LOCAL)
         
-        game_time = game_time.strftime('%B-%d %I:%M%p')
-        
-        team_away = tip_instance.game_team_away
-        team_away_aliases, team_away_id = teamconstants.get_team_aliases(tip_instance.game_sport, league, team_away)
-        team_home = tip_instance.game_team_home
-        team_home_aliases, team_home_id = teamconstants.get_team_aliases(tip_instance.game_sport, league, team_home)
-        
-        wettpoint_h2h_link = 'http://'+constants.SPORTS[tip_instance.game_sport]['wettpoint']+'.'+constants.WETTPOINT_FEED+'/h2h/'+team_home_id+'-'+team_away_id+'.html'
-        
-        wettpoint_stake = tip_instance.wettpoint_tip_stake
-        wettpoint_team = tip_instance.wettpoint_tip_team
-        wettpoint_total = tip_instance.wettpoint_tip_total
-        
-        latest_line, latest_date = tipanalysis.get_line(tip_instance.team_lines)
-        spread_no = tipanalysis.get_line(tip_instance.spread_no)[0]
-        total_no = tipanalysis.get_line(tip_instance.total_no)[0]
-        
-        if latest_date:
-            latest_date = latest_date.replace(tzinfo=pytz.utc).astimezone(local_timezone)
-            latest_date = latest_date.strftime('%Y/%m/%d %I:%M%p')
+        game_count = 0
+        next_games_html.append('<div class="upcoming_games">')
+        for tip_instance in not_archived_tips:
+            game_count += 1
+            game_row_id = league.lower().replace(' ','_')+'-'+str(game_count)
             
-        game_count += 1
-        next_games_html.append('<div class="upcoming_game">')
-        next_games_html.append('<span class="game-count" style="min-width: 16px;">%(game_count)d</span>' % locals())
-        next_games_html.append('<span class="%(date_class)s">%(game_time)s</span>' % locals())
-        next_games_html.append('<span class="participants"><span class="away">%(team_away)s</span> - <span class="home">%(team_home)s</span></span>' % locals())
-        next_games_html.append('<span class="tip-stake">%(wettpoint_stake)s</span>' % locals())
-        next_games_html.append('<span class="tip-team">%(wettpoint_team)s</span>' % locals())
-        next_games_html.append('<span class="tip-line">%(latest_line)s</span>' % locals())
-        next_games_html.append('<span class="tip-line">%(spread_no)s</span>' % locals())
-        next_games_html.append('<span class="tip-date">%(latest_date)s</span>' % locals())
-        next_games_html.append('<span class="tip-total">%(wettpoint_total)s</span>' % locals())
-        next_games_html.append('<span class="tip-total_no">%(total_no)s</span>' % locals())
-        if tip_instance.game_sport not in constants.SPORTS_H2H_EXCLUDE:
-            next_games_html.append('<span class="h2h-link"><a href="%(wettpoint_h2h_link)s">H2H</a></span>' % locals())
+            current_time = datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(local_timezone)
+            game_time = tip_instance.date.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+            
+            date_class = 'game_time-MST'
+            if current_time.date().day == game_time.date().day:
+                date_class += ' today'
+            elif (game_time.date() - current_time.date()).days == 1:
+                date_class += ' tomorrow'
+            if game_time - current_time < timedelta(hours=2):
+                date_class += ' closing'
+            if tip_instance.elapsed == True:
+                date_class += ' pending'
+            
+            game_time = game_time.strftime('%B-%d %I:%M%p')
+            
+            team_away = tip_instance.game_team_away
+            team_away_aliases, team_away_id = teamconstants.get_team_aliases(tip_instance.game_sport, league, team_away)
+            team_home = tip_instance.game_team_home
+            team_home_aliases, team_home_id = teamconstants.get_team_aliases(tip_instance.game_sport, league, team_home)
+            
+            wettpoint_h2h_link = None
+            if (
+                tip_instance.game_sport not in constants.SPORTS_H2H_EXCLUDE 
+                and team_away_id is not None 
+                and team_home_id is not None
+            ):
+                wettpoint_h2h_link = 'http://'+constants.SPORTS[tip_instance.game_sport]['wettpoint']+'.'+constants.WETTPOINT_FEED+'/h2h/'+team_home_id+'-'+team_away_id+'.html'
+            
+            wettpoint_stake = tip_instance.wettpoint_tip_stake
+            wettpoint_team = tip_instance.wettpoint_tip_team
+            wettpoint_total = tip_instance.wettpoint_tip_total
+            
+            latest_line, latest_team_date = tipanalysis.get_line(tip_instance.team_lines)
+            spread_no, latest_spread_date = tipanalysis.get_line(tip_instance.spread_no)
+            total_no, latest_total_date = tipanalysis.get_line(tip_instance.total_no)
+            
+            latest_date = None
+            if latest_team_date is not None:
+                latest_date = latest_team_date
+            elif latest_spread_date is not None:
+                latest_date = latest_spread_date
+            elif latest_total_date is not None:
+                latest_date = latest_total_date
+                
+            if latest_date is not None:
+                latest_date = latest_date.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+                latest_date = latest_date.strftime('%Y/%m/%d %I:%M%p')
+            else:
+                if ' ' in date_class:
+                    self.styleheader.append('#%(game_row_id) {background-color: yellow;}')
+                    
+            next_games_html.append('<div id="%(game_row_id)s" class="upcoming_game">' % locals())
+            next_games_html.append('<span class="game-count" style="min-width: 16px;">%(game_count)d</span>' % locals())
+            next_games_html.append('<span class="%(date_class)s">%(game_time)s</span>' % locals())
+            next_games_html.append('<span class="participants"><span class="away">%(team_away)s</span> - <span class="home">%(team_home)s</span></span>' % locals())
+            next_games_html.append('<span class="tip-stake">%(wettpoint_stake)s</span>' % locals())
+            next_games_html.append('<span class="tip-team">%(wettpoint_team)s</span>' % locals())
+            next_games_html.append('<span class="tip-line">%(latest_line)s</span>' % locals())
+            next_games_html.append('<span class="tip-line">%(spread_no)s</span>' % locals())
+            next_games_html.append('<span class="tip-date">%(latest_date)s</span>' % locals())
+            next_games_html.append('<span class="tip-total">%(wettpoint_total)s</span>' % locals())
+            next_games_html.append('<span class="tip-total_no">%(total_no)s</span>' % locals())
+            if wettpoint_h2h_link is not None:
+                next_games_html.append('<span class="h2h-link"><a href="%(wettpoint_h2h_link)s">H2H</a></span>' % locals())
+            next_games_html.append('</div>')
+        
         next_games_html.append('</div>')
-    
-    next_games_html.append('</div>')
-    
-    return "".join(next_games_html)
+        
+        self.html.append("".join(next_games_html))

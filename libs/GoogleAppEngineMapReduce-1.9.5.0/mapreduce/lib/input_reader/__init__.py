a3995fe646ab40fea457d35d84838e2fcde17fd3
John <nguyen3@ualberta.ca>
2016-11-11 14:53:39 -0700

    re #111: remove old libraries folder

--- libs/GoogleAppEngineMapReduce-1.9.5.0/mapreduce/lib/input_reader/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""Various libraries regarding input readers and input in general."""
-import sys
-sys.path.append('libs/GoogleAppEngineMapReduce-1.9.5.0')
-
-from mapreduce.lib.input_reader._gcs import GCSInputReader
-from mapreduce.lib.input_reader._gcs import GCSRecordInputReader
-from mapreduce.lib.input_reader._gcs import PathFilter
-
